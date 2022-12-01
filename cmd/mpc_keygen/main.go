d5dc9e8fddec3fbccf50a28160eb4baf63eef023
sn0wba <0xsn0wball@gmail.com>
2022-12-01 06:11:56 -0600

    add cmd to start server, client

---
+++ cmd/mpc_keygen/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gotabit/tss-server/pkg/networking/client"
+	"github.com/gotabit/tss-server/pkg/networking/server"
+	"github.com/gotabit/tss-server/pkg/tss_wrap"
+	zerolog "github.com/rs/zerolog/log"
+)
+
+// examples:
+// keygen server:
+// go run main.go -server :8080 -meID A -partyIDs "A B C" -threshold 2 -shareDir /tmp/mpc
+// keygen client:
+// go run main.go -url http://localhost:8080 -meID B -partyIDs "A B C" -threshold 2 -shareDir /tmp/mpc
+
+// reshare server (for new commit) and client (for old committee):
+// go run main.go -reshare -server :8080 -url http://localhost:8080 -meID A -partyIDs "A B C" -threshold 2 -meIdNew A2 -partyIDsNew "A2 B2 C2 D2" -thresholdNew 3 -shareDir /tmp/mpc
+// reshare clients (for both committee):
+// go run main.go -reshare -url http://localhost:8080 -meID B -partyIDs "A B C" -threshold 2 -meIdNew B2 -partyIDsNew "A2 B2 C2 D2" -thresholdNew 3 -shareDir /tmp/mpc
+
+var logger = zerolog.Logger
+
+func main() {
+	flagOperation := flag.Bool("reshare", false, "do reshare (default: keygen)")
+
+	flagServerHost := flag.String("server", "", "if specified, run a server on this port (ex: ':8080')")
+	flagUrl := flag.String("url", "", "url to which a client will connect")
+	flagAccessToken := flag.String("accessToken", "", "url to which a client will connect")
+
+	flagMeID := flag.String("meID", "", "my ID")
+	flagPartyIDs := flag.String("partyIDs", "", "party IDs (space separated)")
+	flagThreshold := flag.Int("threshold", -1, "threshold")
+
+	flagShareDir := flag.String("shareDir", "", "path to directory with shares")
+
+	// for reshare only
+	flagMeIDNew := flag.String("meIDNew", "", "new my ID (for reshare)")
+	flagPartyIDsNew := flag.String("partyIDsNew", "", "new party IDs (space separated) (for reshare)")
+	flagThresholdNew := flag.Int("thresholdNew", -1, "new threshold (for reshare)")
+
+	flag.Parse()
+
+	partyIDs := strings.Split(*flagPartyIDs, " ")
+	isServer := *flagServerHost != ""
+	checkThreshold(*flagThreshold)
+	checkPartyIDs(partyIDs)
+	checkShareDir(*flagShareDir)
+
+	if !*flagOperation {
+		keygen(isServer, *flagServerHost, *flagUrl, *flagAccessToken, *flagMeID, partyIDs, *flagThreshold, *flagShareDir)
+	} else {
+		partyIDsNew := strings.Split(*flagPartyIDsNew, " ")
+		checkThreshold(*flagThresholdNew)
+		checkPartyIDs(partyIDsNew)
+
+		reshareBothCommittee(isServer, *flagServerHost, *flagUrl, *flagAccessToken, *flagMeID, *flagMeIDNew, partyIDs, partyIDsNew, *flagThreshold, *flagThresholdNew, *flagShareDir)
+	}
+}
+
+func keygen(isServer bool, hostUrl, serverURL string, accessToken string, id string, partyIDs []string, threshold int, shareDir string) {
+	sharePath := getSharePath(shareDir, id)
+
+	fmt.Println("=======================================================")
+	fmt.Println("You are about to generate the MPC share")
+	fmt.Println("IDS: ", partyIDs, "; threshold: ", threshold)
+	fmt.Println("Your ID: ", id, "; share path: ", sharePath)
+	fmt.Println("Is this server: ", isServer, "; server URL: ", serverURL)
+	fmt.Println("=======================================================")
+
+	if isShareExist(sharePath) {
+		log.Fatal("share already exist")
+	}
+	mpcc := tss_wrap.NewMpc(id, threshold, &logger)
+	netOperation := createNetworking(isServer, hostUrl, serverURL, accessToken, mpcc)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Minute)
+	err := netOperation.Keygen(ctx, partyIDs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	saveShare(mpcc, sharePath)
+}
+
+func reshare(isServer bool, hostUrl, serverURL, accessToken string, id string, meInNewCommittee bool, partyIDsOld, partyIDsNew []string, thresholdOld, thresholdNew int, shareDir string) {
+	sharePath := getSharePath(shareDir, id)
+
+	fmt.Println("=======================================================")
+	fmt.Println("You are about to reshare the MPC share")
+	fmt.Println("Old IDS: ", partyIDsOld, "; threshold: ", thresholdOld)
+	fmt.Println("New IDS: ", partyIDsNew, "; threshold: ", thresholdNew)
+	fmt.Println("Your ID: ", id, "; share path: ", sharePath)
+	fmt.Println("Is your in new committee: ", meInNewCommittee)
+	fmt.Println("Is this server: ", isServer, "; server URL: ", serverURL)
+	fmt.Println("=======================================================")
+
+	mpcc := tss_wrap.NewMpc(id, thresholdOld, &logger)
+	if !meInNewCommittee {
+		readShare(mpcc, sharePath)
+	}
+
+	netOperation := createNetworking(isServer, hostUrl, serverURL, accessToken, mpcc)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Minute)
+	err := netOperation.Reshare(ctx, partyIDsOld, partyIDsNew, thresholdNew)
+	if err != nil {
+		log.Fatal(err)
+	}
+	saveShare(mpcc, sharePath)
+}
+
+func reshareBothCommittee(isServer bool, serverHost, url, accessToken, meID, meIDNew string, partyIDs, partyIDsNew []string, threshold, thresholdNew int, shareDir string) {
+	var wg sync.WaitGroup
+	if meIDNew != "" { // we are in new committee
+		wg.Add(1)
+		go func() {
+			reshare(isServer, serverHost, url, accessToken,
+				meIDNew, true, partyIDs, partyIDsNew,
+				threshold, thresholdNew, shareDir)
+			wg.Done()
+		}()
+
+		// if new committee just launched as server, old committee must be run as client, coz can't run server twice
+		time.Sleep(2 * time.Second)
+		isServer = false
+	}
+	if meID != "" { // we are in old committee
+		reshare(isServer, serverHost, url, accessToken,
+			meID, false, partyIDs, partyIDsNew,
+			threshold, thresholdNew, shareDir)
+	}
+	wg.Wait()
+}
+
+// utils
+
+type networkingOperations interface {
+	Reshare(ctx context.Context, partyIDsOld, partyIDsNew []string, thresholdNew int) error
+	Keygen(ctx context.Context, party []string) error
+}
+
+func createNetworking(isServer bool, hostUrl string, serverURL string, accessToken string, mpcc *tss_wrap.Mpc) networkingOperations {
+	if isServer {
+		server_ := server.NewServer(mpcc, accessToken, &logger)
+		go http.ListenAndServe(hostUrl, server_)
+		return server_
+	} else {
+		return client.NewClient(mpcc, serverURL, accessToken, &logger)
+	}
+}
+
+// share file utils
+
+func saveShare(tss *tss_wrap.Mpc, sharePath string) {
+	if share, err := tss.Share(); err != nil {
+		log.Fatal(fmt.Errorf("can't get share: %w", err))
+	} else if err = os.WriteFile(sharePath, share, 0644); err != nil {
+		log.Fatal(fmt.Errorf("can't save share: %w", err))
+	}
+}
+
+func readShare(tss *tss_wrap.Mpc, sharePath string) {
+	if share, err := os.ReadFile(sharePath); err != nil {
+		log.Fatal(fmt.Errorf("can't read share: %w", err))
+	} else if err = tss.SetShare(share); err != nil {
+		log.Fatal(fmt.Errorf("can't set share: %w", err))
+	}
+}
+
+func isShareExist(sharePath string) bool {
+	_, err := os.Stat(sharePath)
+	return err == nil
+}
+func getSharePath(shareDir, id string) string {
+	return filepath.Join(shareDir, fmt.Sprintf("share_%s", id))
+}
+
+// flags checks
+
+func checkShareDir(dirPath string) {
+	fileInfo, err := os.Stat(dirPath)
+	if err != nil {
+		log.Fatalf("something wring with dirPath (%v): %v", dirPath, err)
+	}
+	if !fileInfo.IsDir() {
+		log.Fatalf("dirPath (%v) is not a directory", dirPath)
+	}
+}
+
+func checkThreshold(t int) {
+	if t < 2 {
+		log.Fatal("threshold must be >= 2")
+	}
+}
+func checkPartyIDs(partyIDs []string) {
+	if len(partyIDs) < 2 {
+		log.Fatal("partyIDs must be >= 2")
+	}
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.19
 
 require (
 	github.com/ethereum/go-ethereum v1.10.26
-	github.com/gin-gonic/gin v1.8.1
 	github.com/gorilla/websocket v1.4.2
 	github.com/rs/zerolog v1.28.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
@@ -22,34 +21,23 @@ require (
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.0.0 // indirect
 	github.com/ipfs/go-log v0.0.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/otiai10/primes v0.0.0-20180210170552-f6d2a1ba97c4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/whyrusleeping/go-logging v0.0.0-20170515211332-0457bb6b88fc // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
 	github.com/bnb-chain/tss-lib v1.3.5
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
-	github.com/goccy/go-json v0.9.7 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/stretchr/testify v1.8.1
-	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
